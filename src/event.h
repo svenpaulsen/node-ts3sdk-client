e5273e78e2cc5b6a759a38c151998ed540672d64
Sven Paulsen <info@sven-paulsen.de>
2017-09-25 19:05:08 +0200

    removed non-implemented events

--- src/event.h
+++ src/event.h
@@ -55,18 +55,7 @@ public:
     static void onChannelPasswordChangedEvent(uint64 scHandlerID, uint64 channelID);
     static void onPlaybackShutdownCompleteEvent(uint64 scHandlerID);
     static void onSoundDeviceListChangedEvent(const char* modeID, int type);
-    static void onEditPlaybackVoiceDataEvent(uint64 scHandlerID, anyID clientID, short* samples, int sampleCount, int channels);
-    static void onEditPostProcessVoiceDataEvent(uint64 scHandlerID, anyID clientID, short* samples, int sampleCount, int channels, const unsigned int* channelSpeakerArray, unsigned int* channelFillMask);
-    static void onEditMixedPlaybackVoiceDataEvent(uint64 scHandlerID, short* samples, int sampleCount, int channels, const unsigned int* channelSpeakerArray, unsigned int* channelFillMask);
-    static void onEditCapturedVoiceDataEvent(uint64 scHandlerID, short* samples, int sampleCount, int channels, int* edited);
-    static void onCustom3dRolloffCalculationClientEvent(uint64 scHandlerID, anyID clientID, float distance, float* volume);
-    static void onCustom3dRolloffCalculationWaveEvent(uint64 scHandlerID, uint64 waveHandle, float distance, float* volume);
     static void onUserLoggingMessageEvent(const char* message, int logLevel, const char* logChannel, uint64 logID, const char* logTime, const char* formattedString);
-    static void onCustomPacketEncryptEvent(char** data, unsigned int* size);
-    static void onCustomPacketDecryptEvent(char** data, unsigned int* size);
-    static void onProvisioningSlotRequestResultEvent(unsigned int error, uint64 requestHandle, const char* connectionKey);
-    static void onCheckServerUniqueIdentifierEvent(uint64 scHandlerID, const char* serverUID, int* cancelConnect);
-    static void onClientPasswordEncrypt(uint64 scHandlerID, const char* plain, char* encrypted, int size);
     static void onFileTransferStatusEvent(anyID transferID, unsigned int status, const char* statusMessage, uint64 remoteSize, uint64 scHandlerID);
     static void onFileListEvent(uint64 scHandlerID, uint64 channelID, const char* path, const char* name, uint64 size, uint64 datetime, int type, uint64 incompleteSize, const char* returnCode);
     static void onFileListFinishedEvent(uint64 scHandlerID, uint64 channelID, const char* path);
